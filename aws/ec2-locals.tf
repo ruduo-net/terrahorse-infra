bcab2fbab32087b1d8ea0b253ca6852542330cf8
Putinas Piliponis <putinas.piliponis@ruduo.net>
2026-08-26 08:57:53 +0300

    Isolate EC2 roles by environment

--- aws/ec2-locals.tf
+++ aws/ec2-locals.tf
@@ -41,3 +41,33 @@ moved {
   from = aws_autoscaling_group.ec2
   to   = aws_autoscaling_group.ec2["dev"]
 }
+
+moved {
+  from = aws_iam_role.ec2
+  to   = aws_iam_role.ec2["dev"]
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.ec2-ssm
+  to   = aws_iam_role_policy_attachment.ec2-ssm["dev"]
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.ec2-ecr-read-only
+  to   = aws_iam_role_policy_attachment.ec2-ecr-read-only["dev"]
+}
+
+moved {
+  from = aws_iam_role_policy.ec2-volume
+  to   = aws_iam_role_policy.ec2-volume["dev"]
+}
+
+moved {
+  from = aws_iam_role_policy.ec2-parameters
+  to   = aws_iam_role_policy.ec2-parameters["dev"]
+}
+
+moved {
+  from = aws_iam_instance_profile.ec2
+  to   = aws_iam_instance_profile.ec2["dev"]
+}
